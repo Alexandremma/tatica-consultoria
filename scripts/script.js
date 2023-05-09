42aac1b61d551fd75bf29228ead5ce2271818807
ammac <alexandremmac@gmail.com>
2023-05-09 17:06:52 -0300

    mobile-version and cases and articles section

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+const openMenuButton = document.getElementById("mobile-open-menu-icon");
+const closeMenuButton = document.getElementById("mobile-close-menu-icon");
+
+const mobileMenu = document.getElementById("mobile-nav-menu-container");
+const mobileMenuList = document.getElementsByClassName("mobile-menu-list");
+const mobileMenuItems = document.querySelectorAll(".mobile-nav-menu-container .mobile-menu-list .mobile-menu-item")
+
+openMenuButton.addEventListener('click', () => {
+    openMenuButton.style.display = 'none';
+    closeMenuButton.style.display = 'flex';
+    mobileMenu.style.width = '100%';
+    mobileMenuList[0].style.display = 'flex';
+});
+
+closeMenuButton.addEventListener('click', () => {
+    closeMenuButton.style.display = 'none';
+    openMenuButton.style.display = 'flex';
+    mobileMenu.style.width = '0';
+    mobileMenuList[0].style.display = 'none';
+});
+
+mobileMenuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        closeMenuButton.style.display = 'none';
+        openMenuButton.style.display = 'flex';
+        setTimeout(() => {
+            mobileMenu.style.width = '0';
+            mobileMenuList[0].style.display = 'none';
+        }, 300);
+    });
+});
